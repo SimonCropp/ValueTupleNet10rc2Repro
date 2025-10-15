f8e44322446dc617eac54473092a8cbbabcc3e7d
Simon Cropp <simon.cropp@gmail.com>
2025-10-15 21:10:47 +1100

    .

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+#pragma warning disable AppendParameter
+static class FileNameBuilder
+{
+
+    public static (Action<StringBuilder>?, Action<StringBuilder>?) GetParameterText()
+    {
+        return (null, null);
+    }
+
+}
